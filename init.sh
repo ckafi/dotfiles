ef33a98ae4ac94b13e1c428960fa83dcd2cf6fad
Tobias Frilling <tobias@frilling-online.de>
2015-02-01 14:50:45 +0100

    Remove superfluous dir from init

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # run this script to initialize this repo
 
-mkdir backup/ undo/ swap/ bundle/ autoload/
+mkdir backup/ undo/ swap/ autoload/
 curl -fLo autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
