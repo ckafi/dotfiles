a475ab88e9cf4c70d8ad1f87203446dc10d1b45d
Tobias Frilling <tobias@frilling-online.de>
2015-07-31 15:34:18 +0200

    Add leiningen config

---
+++ lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-ancient "0.6.1"]
+                  [venantius/ultra "0.1.9"]
+                  [lein-kibit "0.0.8"]
+                  [lein-bikeshed "0.2.0"]
+                  [lein-cljfmt "0.1.10"]]}}
