4f47da449de7d549b0bdb598ca041376bdd6f17b
Tobias Frilling <tobias@frilling-online.de>
2016-08-26 23:35:57 +0200

    Update lein profile deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,4 @@
                   [venantius/ultra "0.4.1"]
                   [lein-kibit "0.1.2"]
                   [lein-bikeshed "0.3.0"]
-                  [lein-cljfmt "0.5.2"]]}}
+                  [lein-cljfmt "0.5.3"]]}}
