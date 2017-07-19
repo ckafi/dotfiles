7cc8f1a70c9a67aec276fae5c06c517635e3cca9
Tobias Frilling <tobias@frilling-online.de>
2017-07-19 13:50:29 +0200

    Update lein plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
-                  [venantius/ultra "0.4.1"]
-                  [lein-kibit "0.1.2"]
-                  [lein-bikeshed "0.3.0"]
-                  [lein-cljfmt "0.5.3"]]}}
+                  [venantius/ultra "0.5.1"]
+                  [lein-kibit "0.1.5"]
+                  [lein-bikeshed "0.4.1"]
+                  [lein-cljfmt "0.5.6"]]}}
