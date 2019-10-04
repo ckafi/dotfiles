bac4f2ed4bad8cd6d1ef9b9bb9bf91686fc870d0
Tobias Frilling <tobias@frilling-online.de>
2019-10-04 17:46:14 +0200

    Move nvim config

--- init.sh
+++
@@ -1,9 +0,0 @@
-REPODIR=$PWD
-cd ~
-mkdir -p .config/nvim
-cd .config/nvim
-ln -s $REPODIR/* .
-mkdir autoload/
-mkdir ~/.local/share/nvim/{backup,undo,swap}/
-curl -fLo autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugInstall
