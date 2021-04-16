8838517d3924dc6ef3afad28edb18ccb8e052545
Tobias Frilling <tobias@frilling-online.de>
2021-04-16 10:32:56 +0200

    [lein] Remove dotfiles

--- lein/profiles.clj
+++
@@ -1,5 +0,0 @@
-{:user {:plugins [[lein-ancient "0.6.15"]
-                  [venantius/ultra "0.6.0"]
-                  [lein-kibit "0.1.6"]
-                  [lein-bikeshed "0.5.2"]
-                  [lein-cljfmt "0.6.4"]]}}
