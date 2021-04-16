4285f0d1dcaaae7b51b614936126a2c9983f1aa9
Tobias Frilling <tobias@frilling-online.de>
2021-04-16 10:35:38 +0200

    Remove init.zsh

--- init.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-export ZDOTDIR=${HOME}/.zsh/
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-dotdrop install
