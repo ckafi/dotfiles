4cfa3b6203facd12db67b786ebf3e320a2521ac4
Tobias Frilling <tobias@frilling.dev>
2021-04-30 15:29:59 +0200

    [nvim] ignore overfull warnings in latex

--- config/nvim/lua/config/vimtex.lua
+++ config/nvim/lua/config/vimtex.lua
@@ -2,5 +2,6 @@ vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_compiler_progname = 'nvr'
 vim.g.vimtex_quickfix_ignore_filters = {
   'contains only floats',
-  'multiply.defined'
+  'multiply.defined',
+  'Overfull',
 }
