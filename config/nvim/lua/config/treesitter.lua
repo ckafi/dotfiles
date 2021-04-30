aa4650f0058dc581b1d5f3e17182f4b7be83a53d
Tobias Frilling <tobias@frilling.dev>
2021-04-30 15:29:31 +0200

    [nvim] disable treesitter highlighting for latex

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,18 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all",
-  highlight = { enable = true, },
+  highlight = {
+    enable = true,
+    disable = { "latex" }
+  },
   textobjects = { enable = true },
   indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
